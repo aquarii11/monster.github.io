3136929469f6fcc69e556ed467fd513d5392bd3f
Rameel Azhungal Chellattan Rehim <rameelster@gmail.com>
2019-12-28 01:11:59 +0400

    Updates

---
+++ precache-manifest.15b67fa6d1e6776513a7ee6561142902.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755d80f723ecedf90821138bf6da0e30",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "13e7512418c36a13fdc6",
+    "url": "/monster/static/css/main.3cba644d.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/monster/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "13e7512418c36a13fdc6",
+    "url": "/monster/static/js/main.de953330.chunk.js"
+  },
+  {
+    "revision": "306bab9e98ae9683c6a7",
+    "url": "/monster/static/js/runtime-main.5b07adb3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monster/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters/precache-manifest.5c3f65dd01115eeda5842e9565a0e1a4.js"
+  "/monster/precache-manifest.15b67fa6d1e6776513a7ee6561142902.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monster/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
