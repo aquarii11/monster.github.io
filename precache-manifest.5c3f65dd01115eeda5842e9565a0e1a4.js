07b4fa6106a7b29aac80c6f89c17ef52bb495d01
Rameel Azhungal Chellattan Rehim <rameel.rehim@alshaya.com>
2019-12-28 00:56:02 +0400

    Updates

---
+++ precache-manifest.5c3f65dd01115eeda5842e9565a0e1a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1737598d8a5572f6cd7917abc6ee3c97",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "d8304f08386d9555ce75",
+    "url": "/monsters/static/css/main.3cba644d.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/monsters/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "d8304f08386d9555ce75",
+    "url": "/monsters/static/js/main.e8564a60.chunk.js"
+  },
+  {
+    "revision": "b29c299a2556335a6eb0",
+    "url": "/monsters/static/js/runtime-main.02df2f2e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters/static/media/logo.25bf045c.svg"
+  }
+]);
